d561595e12c99893c5bc80d1c7b5f63735553f5e
Shivi Gupta <shivigupta31121988@gmail.com>
2021-02-04 00:48:56 +0530

    Create sqlSchema.sql

---
+++ sqlSchema.sql
@@ -0,0 +1,4 @@
+create table tbl_emp (first_name varchar(50), last_name varchar(50),
+                      empID int Primary Key, salary float)
+
+insert into tbl_emp values('Shivi','Gupta',1001,2500.50)
